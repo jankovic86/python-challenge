31ffe7fef69dec9333583bb32ed4d22b799db631
Nikola Jankovic <nikolajankovic@Nikolas-MacBook-Pro.local>
2021-07-05 17:01:51 -0400

    Script without results text file.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -29,3 +29,31 @@ with open(csvpath) as csvfile:
 
         elif (row[2] == "O'Tooley"):
             otooley += 1
+        khan_percent = khan / total
+
+    correy_percent = correy / total
+    li_percent = li / total
+    otooley_percent = otooley / total
+
+    election_winner = max(khan, correy, li, otooley)
+
+    if election_winner == khan:
+        winner_name = "Khan"
+    elif election_winner == correy:
+        winner_name = "Correy"
+    elif election_winner == li:
+        winner_name = "Li"
+    elif election_winner == otooley:
+        winner_name = "O'Tooley"
+
+print(f"Election Results")
+print(f"------------------------------")
+print(f"Total Votes: {total}")
+print(f"------------------------------")
+print(f"Khan: {khan_percent} ({khan})")
+print(f"Correy: {correy_percent} ({correy})")
+print(f"Khan: {li_percent} ({li})")
+print(f"Khan: {otooley_percent} ({otooley})")
+print(f"------------------------------")
+print(f"Winner: {winner_name}")
+print(f"------------------------------")
