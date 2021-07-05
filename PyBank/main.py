f2e42c857a5fd771979fa0f6b29c9d0a4dcb47cd
Nikola Jankovic <nikolajankovic@Nikolas-MacBook-Pro.local>
2021-07-05 03:23:38 -0400

    Script without the text file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,56 @@
 import os
 import csv
 
+
 csvpath = os.path.join("..", "PyBank", "Resources", "budget_data.csv")
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile)
     csv_header = next(csvreader)
-    months = len(list(csvreader))
-    print(months)
+    row = next(csvreader)
+
+    dates = []
+    change = []
+    months = 0
+    total = 0
+    increase = 0
+    increase_month = 0
+    decrease = 0
+    decrease_month = 0
+
+    months += 1
+    previous_row = int(row[1])
+    total += int(row[1])
+    increase = int(row[1])
+    increase_month = row[0]
+
+    for row in csvreader:
+        months += 1
+        total += int(row[1])
+        revenue_change = int(row[1]) - previous_row
+        change.append(revenue_change)
+        previous_row = int(row[1])
+        dates.append(row[0])
+
+        if int(row[1]) > increase:
+            increase = int(row[1])
+            increase_month = row[0]
+
+        if int(row[1]) < decrease:
+            decrease = int(row[1])
+            decrease_month = row[0]
+
+    average = sum(change) / len(change)
+
+    highest_amount = max(change)
+    lowest_amount = min(change)
+
+print("Financial Analysis")
+print("---------------------------")
+print("Total Months: ", + months)
+print("Total: ", + total)
+print("Average Change: $", + average)
+print("Greatest Increase in Profits: " +
+      increase_month, "$", highest_amount)
+print("Greatest Decrease in Profits: " +
+      decrease_month,  "$", lowest_amount)
