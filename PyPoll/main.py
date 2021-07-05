9210c95c98dd1d4440c115b74c7896113eb4a542
Nikola Jankovic <nikolajankovic@Nikolas-MacBook-Pro.local>
2021-07-05 16:26:44 -0400

    PyPoll script to search and count votes.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,4 +8,24 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile)
     csv_header = next(csvreader)
 
-    print(csvreader)
+    total = 0
+    khan = 0
+    correy = 0
+    li = 0
+    otooley = 0
+
+    for row in csvreader:
+
+        total += 1
+
+        if (row[2] == "Khan"):
+            khan += 1
+
+        elif (row[2] == "Correy"):
+            correy += 1
+
+        elif (row[2] == "Li"):
+            li += 1
+
+        elif (row[2] == "O'Tooley"):
+            otooley += 1
